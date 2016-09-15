f24f18c62f78595e00af23624e693695c3fa6810
Chan Yi <huichanyi@gmail.com>
2016-09-14 22:36:52 -0400

    Add mongo connection and index logic

--- server/app.js
+++ server/app.js
@@ -4,10 +4,18 @@ let express = require('express');
 let path = require('path');
 let app = express();
 
+let mongoUtil = require('./mongoUtil');
+mongoUtil.connect();
+
 app.use(express.static(path.join(__dirname, '../client')));
 
 app.get("/sports", (req, res) => {
-	res.json(["Cycling", "Weightlifting"]);
+	let sports =  mongoUtil.sport();
+	sports.find().toArray((err, docs)=> {
+		let sportsNames = docs.map((sport) => sport.name);
+		res.json(sportsNames);	
+	});		
+	//res.json(["Cycling", "Weightlifting"]);
 });
 
 app.listen(8181, () => console.log('Listening to 8181'));
