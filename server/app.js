dbb62d69d67fd88acb247b1a8b5bbf17820d5390
Chan Yi <huichanyi@gmail.com>
2016-09-21 21:39:29 -0400

    Added Mongo DB for Individual Sport

--- server/app.js
+++ server/app.js
@@ -18,4 +18,18 @@ app.get("/sports", (req, res) => {
 	//res.json(["Cycling", "Weightlifting"]);
 });
 
+app.get("/sports/:name", (req, res) => {
+	let sportName = req.params.name;
+	let sports = mongoUtil.sport();
+	sports.find({name: sportName}).limit(1).next((err, doc) => {
+		if(err) {
+			res.status(500).json(err);
+		}	
+		res.json(doc);
+	});
+	console.log("Sport name: ", sportName);
+
+
+});
+
 app.listen(8181, () => console.log('Listening to 8181'));
